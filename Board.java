227efb93f5880566fb36ab8b59b1d592eee25c09
AR22-cr <adhithya.rrk@gmail.com>
2025-10-03 16:44:50 -0400

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,1 +1,18 @@
 System.out.println("This is the board!!!");
+public void paintPolygon(Graphics brush, Polygon p) {
+	   Point[] points = p.getPoints();
+	   int [] xPoints = new int[points.length];
+	   int [] yPoints = new int[points.length];
+	   
+	   for (int i = 0; i < points.length; i++) {
+		   int x = (int) points[i].x;
+		   int y = (int) points[i].y;
+		   
+		   xPoints[i] = x;
+		   yPoints[i] = y;
+		   
+	   }
+	   brush.drawPolygon(xPoints, yPoints, points.length);	
+	   brush.fillPolygon(xPoints, yPoints, points.length);
+	   
+   }
