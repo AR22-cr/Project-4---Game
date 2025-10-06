5286f49bf136a214fa2ca1a0d0e10d0ae54b4c3c
AR22-cr <adhithya.rrk@gmail.com>
2025-10-06 12:46:13 -0400

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,38 @@
+package game;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Spaceship extends Polygon implements KeyListener {
+	private int numLives;
+	private Powerup powerup; //Make PowerUp class
+	
+	public Spaceship() {
+		super(inShape, inPosition, inRotation);
+		numLives = 3;
+		powerup = new Powerup(nothing)
+	}
+	
+	public int getLives() {
+		return numLives;
+	}
+	
+	public void loseLife() {
+		numLives -= 1;
+	}
+	
+	public void collectPowerUp(Powerup powerup) {
+		this.powerup = powerup;
+	}
+	
+	public void keyPressed(KeyEvent e) {
+		//implement
+	}
+	
+	public void keyReleased(KeyEvent e) {
+		//implement
+	}
+	
+	public void keyTyped(KeyEvent e) {
+		//leave empty
+	}
+}
