4df3b8241d7c0572b2fe1b7fbe6c37dcf5faf0c6
smallon-beep <smallon@terpmail.umd.edu>
2025-10-15 12:30:40 -0400

    Update PowerUp.java

--- PowerUp.java
+++ PowerUp.java
@@ -2,7 +2,7 @@ package game;
 
 public class PowerUp extends Polygon{
 	public PowerUp(int rand){
-		super(rand == 0 ? (new Point[] {new Point(0,0), new Point(25,0), new Point(25,25), new Point(0,25)}) : (new Point[] {new Point(25,0), new Point(50,25), new Point(0,25)}), new Point(375,0), 0);
+		super(rand == 0 ? (new Point[] {new Point(10,25), new Point(25,25), new Point(30,10), new Point(35,25), new Point(50,25), new Point(40, 35), new Point(45, 50), new Point(30, 40), new Point(15, 50), new Point(20, 35)}) : (new Point[] {new Point(25,0), new Point(50,25), new Point(0,25)}), new Point(375,0), 0);
 		
 		int lane = (int) (Math.random()*3);
 		if (lane == 0) {
