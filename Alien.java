683e16f3e5147d177b46ac5ced82b3f8fcbcbf68
AR22-cr <adhithya.rrk@gmail.com>
2025-10-16 16:00:27 -0400

    Update Alien.java

--- Alien.java
+++ Alien.java
@@ -1,1 +1,44 @@
+package game;
+
+public class Alien extends Polygon implements Obstacle {
+	private int lane;
+	private int speed;
+	
+	public Alien(int rand) {
+		super(new Point[] {new Point(25,0), new Point(50, 25), new Point(0,25), }, 
+				new Point(375, 0), 0);
+		lane = rand;
+		if (lane == 0) {
+			position.x = 75;
+		}
+		if (lane == 1) {
+			
+		}
+		
+		if (lane == 2) {
+			position.x = 625;
+		}	
+		speed = 4;
+	}
+	
+	@Override
+	public boolean checkCollision(Spaceship s) {
+		if (this.collides(s)) {
+			s.loseLife();
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public int getSpeed() {
+		return speed;
+		
+	}
+	
+	public void rotateAlien() {
+		this.rotate(30);
+		
+	}
+}
 
